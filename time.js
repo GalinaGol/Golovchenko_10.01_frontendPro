f101b9cfd234ed99285d6fe49ae89bc750ecf1ff
Galina Gol <galyna.golovchenko@gmail.com>
2023-01-20 23:40:44 +0200

    hw4

---
+++ time.js
@@ -0,0 +1,8 @@
+// let hour = prompt("введіть кількість годин", 2);
+// let sec=3600;
+// let result = hour * sec;
+// alert(`У ${hour} годинах  ${result} секунд.`);   
+
+let hour = prompt("Enter your age!", "");
+const result = hour * 7;
+alert(`In 7 years you will be  ${result} years old.`); 
