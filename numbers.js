dcd568eaf4b6ff6fb016036d7ae57f4d6cdc7052
Galina Gol <galyna.golovchenko@gmail.com>
2023-01-20 23:46:32 +0200

    hw6

---
+++ numbers.js
@@ -0,0 +1,16 @@
+// Завдання 1
+
+let number1 = prompt (String ("введіть перше число", 1));
+let number2 = prompt (String("введіть друге число", 2));
+let number3 = prompt (String("введіть третє число", 3));
+const result = number1 + number2 + number3;
+alert(`Ваше число ${ result }.`)
+
+
+// Завдання 2
+
+const wholeString = "12345";
+
+const splitString = wholeString.split("");
+
+console.log(splitString);
