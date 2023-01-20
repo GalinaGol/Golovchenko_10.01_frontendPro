cbd3063002562da00f76aabb4abfad3f521bfcd3
Galina Gol <galyna.golovchenko@gmail.com>
2023-01-20 23:46:32 +0200

    fixed hw

---
+++ numbers.js
@@ -0,0 +1,12 @@
+// Завдання 1
+
+let firstWord = prompt (String ("введіть перше слово"));
+let secondWord = prompt (String("введіть друге слово"));
+let thirdWord = prompt (String("введіть третє слово"));
+console.log(firstWord+secondWord+thirdWord);
+
+
+// Завдання 2
+
+const wholeString = "12345";
+console.log(`${wholeString[0]} ${wholeString[1]} ${wholeString[2]} ${wholeString[3]} ${wholeString[4]}`);
