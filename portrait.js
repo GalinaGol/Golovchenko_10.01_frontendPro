8bf207172fd417547f3932e2bc199df1cd09c45f
Galina Gol <galyna.golovchenko@gmail.com>
2023-01-20 23:49:17 +0200

    hw8

---
+++ portrait.js
@@ -0,0 +1,42 @@
+
+let yearBirth = prompt("Введіть ваш рік народження", "");
+let city = prompt("Введіть назву міста, в якому ви живете", "");
+let favouriteSport = prompt("Введіть улюбленний вид спорту", "");
+let age = null;
+
+
+if (null === yearBirth) {
+    alert("Шкода, що Ви не хочете сказати скільки Вам рочків. Я знаю, що багато...");
+} else {
+    age = 2023 - yearBirth
+}
+
+switch (city) {
+    case 'Київ':
+        capital = "Ти живеш в столиці України, Слава Україні!";
+        break;
+    case 'Вашингтон':
+        capital= "Ти живеш в столиці CША, де Patriot?";
+        break;
+    case 'Лондон':
+        capital = "London is the capital of GB.";
+        break;
+    default:
+        alert("Шкода, що Ви не захотіли ввести назву міста, в якому ви живете. Це звісно ж не Київ, не Вашингтон та навіть не Лондон");
+}
+
+switch (favouriteSport) {
+    case 'футбол':
+        choosenSport = "як Мессі?";
+        break;
+    case 'бокс':
+        choosenSport= "як брати Кличко?";
+        break;
+    case 'теніс':
+        choosenSport = "як Серена Уільямс?";
+        break;
+    default:
+        alert("Шкода, що Ви не захотіли ввести вид спорту, який вам подобається.");
+}
+
+alert(`Дивись, ти вже немолодий, тобі ${ age }. ${ capital } Круто! Хочеш стати ${ choosenSport } `);
