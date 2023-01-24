0ffb9d4ff6f029a39d360d19a9833c3bf78ee970
Galina Gol <galyna.golovchenko@gmail.com>
2023-01-25 00:29:34 +0200

    fixed vars

--- calc.js
+++ calc.js
@@ -1,29 +1,25 @@
 
-let opt = String(prompt ("выберите одну из функций: + - * /", "")) ;
-
-const add = "+";
-const sub = "-";
-const mult ="*";
-const div = "/";
-
+const opt = String(prompt ("выберите одну из функций: add, sub, mult, div", "")) ;
 let num1 = Number(prompt ("введите первое число", 1));
 let num2 = Number(prompt ("введите второе число", 2));
 
+
+let result = "";
+
 switch (opt) {
-    case add:
+    case 'add':
         result = num1 + num2;
         break;
-    case sub:
+    case 'sub':
         result = num1 - num2;
         break;
-    case mult:
+    case 'mult':
         result = num1 * num2;
         break;
-    case div:
+    case 'div':
         result = num1 / num2;
         break;
     default:
-        alert("Жулик!Считай сам на калькуляторе");
+        alert("Действие выполнить невозможно.");
 }
-
-alert(`я сделяль: ${ num1 } ${ opt } ${ num2 } = ${ result } `);
+alert(`Результат: ${ result }`);
